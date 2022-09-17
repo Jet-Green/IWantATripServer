21a1102ebd28aba9caa781f7150d3cd27decfae5
Jet-Green <grishadzyin@gmail.com>
2022-09-17 14:34:39 +0400

    add uploading

--- middleware/multer-middleware.js
+++ middleware/multer-middleware.js
@@ -2,23 +2,24 @@ const multer = require('multer')
 const fs = require('fs')
 
 
-const storage = multer.diskStorage({
-    dest: './uploads/',
-    //  function (req, file, cb) {
-    //     let dir = '/uploads/';
 
-    //     if (!fs.existsSync(dir)) {
-    //         fs.mkdirSync(dir, { recursive: true });
-    //     }
-    //     cb(null, dir)
-    // },
-    // filename: function (req, file, cb) {
-    //     const uniqueSuffix = Math.round(Math.random() * 1E9) + '.png'
-    //     cb(null, file.fieldname + '_' + uniqueSuffix)
-    // }
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        let dir = `./uploads`;
+
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        cb(null, dir)
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Math.round(Math.random() * 1E9) + '.png'
+        console.log(file);
+        cb(null, file.fieldname + '_' + uniqueSuffix)
+    }
 })
 
 
-const upload = multer({ dest: './uploads' })
+const upload = multer({ storage: storage })
 
 module.exports = upload
