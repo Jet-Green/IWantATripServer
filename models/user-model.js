1d08b704a6cc51754bb633f22faa567f67f38829
Jet-Green <grishadzyin@gmail.com>
2022-11-13 13:42:00 +0400

    ch

--- models/user-model.js
+++ models/user-model.js
@@ -3,9 +3,12 @@ const { Schema, model } = require('mongoose');
 const UserSchema = new Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    fullinfo: {},
     fullname: {
         type: String, required: true
-    }
+    },
+    trips: ['hgfyufdgshuifghiagfdhifgadihfd', 'gfajhguyfdjhkfgahjfdgafdgjkh'],
+    guideElements: ['dfsjfdskjldfsjlkjdksfjkfgds', 'sdjklfjskglkjlfgjklfdgjklfg']
 })
 
 module.exports = model('User', UserSchema);
