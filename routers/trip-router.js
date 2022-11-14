b241c574b5aff8f07c3f543adea45e86f23fcf55
Jet-Green <grishadzyin@gmail.com>
2022-11-14 19:59:23 +0400

    block unauthorized

--- routers/trip-router.js
+++ routers/trip-router.js
@@ -4,6 +4,8 @@ const Router = require('express').Router
 // here all controllers
 const tripController = require('../controllers/trip-controller')
 
+const authMiddleware = require('../middleware/auth-middleware')
+
 const upload = require('../middleware/multer-middleware')
 
 const router = Router()
@@ -13,7 +15,7 @@ const router = Router()
 router.get('/get-all', tripController.getAll)
 router.get('/get-by-id', tripController.getById)
 router.post('/delete-by-id', tripController.deleteById)
-router.post('/create', tripController.create)
+router.post('/create', authMiddleware, tripController.create)
 router.post('/upload-images', upload.any(), tripController.uploadImages)
 router.get('/clear', tripController.clear)
 
