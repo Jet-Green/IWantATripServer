a4b2fd26df8ae0f298d7070f3efa32af5f605f11
qbclub <17926057+qbclub@users.noreply.github.com>
2023-04-02 12:50:56 +0400

    booking model update

--- models/booking-model.js
+++ models/booking-model.js
@@ -1,17 +1,17 @@
 const { Schema, model } = require('mongoose')
 
 const BookingSchema = new Schema({
-    duration: { type: String },
-    name: { type: String },
-    delivery: { type: Boolean },
+
     type: { type: Array },
     start: { type: Number },
     end: { type: Number },
-    resource: { type: String },
-    desc: { type: String },
-    fromAge: { type: String },
-    phoneNumber: { type: String },
-    tripType: { type: String },
+    location:{ type: String },
+    duration: { type: String },
+    adults: { type: Number },
+    children: { type: Number },
+    fromAge: { type: Number },
+    wishes: { type: String },
+    creatorId:{ type:String }
 })
 
 module.exports = model('Booking', BookingSchema);
