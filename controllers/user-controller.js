71ee1e4468503ee013496c56d9ae9266538c8f81
JetGreen <grishadzyin@gmail.com>
2023-06-03 20:45:46 +0400

    if token exists but incorrect

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -44,7 +44,6 @@ module.exports = {
             res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
             return res.json(userData)
         } catch (error) {
-            console.log(error);
             next(error)
         }
     },
