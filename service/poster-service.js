f59849d42003e064d4edb33949f4beb4a818664f
tooortttt <savelijsutov@gmail.com>
2023-06-06 15:58:21 +0400

    posters get all

--- service/poster-service.js
+++ service/poster-service.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose')
+const {Schema} = require('mongoose')
 
-const connection = mongoose.createConnection(process.env.POSTERS_DB_URL);
-const PostersModel = connection.model('Poster', PosterSchema);
+const conn = mongoose.createConnection(process.env.POSTERS_DB_URL);
+const PostersModel = conn.model('Poster', new Schema({
+    image: { type: String },
+    title: { type: String },
+    eventLocation: { type: Object },
+    site: { type: String },
+    organizer: { type: String },
+    phone: { type: String },
+    email: { type: String },
+    date: { type: Number },
+    workingTime: { type: String },
+    eventType: { type: String },
+    isModerated: { type: Boolean, default: false },
+}));
+
+module.exports = conn;
 
 module.exports = {
     async getAll() {
-        console.log(await PostersModel.find({}))
-        return PostersModel.find({})
-
+        try {
+            console.log(await PostersModel.find({}))
+            return PostersModel.find({})
+        } catch (error) {
+            handleError(error);
+        }
     }
 }
