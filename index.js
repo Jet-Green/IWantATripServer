73f88da3320d54004358a4e1fa755686f1017069
qbclub <17926057+qbclub@users.noreply.github.com>
2023-06-28 08:39:00 +0400

    add helmet

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // here all imports ${process.env.NODE_ENV}
 require('dotenv').config({ path: `${process.argv[process.argv.length - 1]}.env` })
 const express = require('express');
+import helmet from "helmet";
+
 const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
@@ -22,7 +24,7 @@ const posterRouter = require('./routers/poster-router')
 
 
 app.use(history())
-
+app.use(helmet());
 
 // here all .use
 app.use(cors({
