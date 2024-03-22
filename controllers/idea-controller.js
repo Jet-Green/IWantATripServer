f904a847a21313043306b6fedb44b5dd986e6514
Yegor Knyazev <egorknyazev10@gmail.com>
2024-03-22 18:25:43 +0400

    added sending ideas

--- controllers/idea-controller.js
+++ controllers/idea-controller.js
@@ -1,9 +1,11 @@
 const ideaModel = require("../models/idea-model")
+const { sendPost } = require("../service/telegram-service")
 
 module.exports = {
     async createIdea(req, res, next) {
         try {
-            await ideaModel.create(Object.assign(req.body, { author: req.user._id }))
+            let idea = await ideaModel.create(Object.assign(req.body, { author: req.user._id }))
+            await sendPost(`<b>Новая идея!</b> <b>Тема:</b> ${idea.topic} ${idea.description}`)
             res.sendStatus(200)
         } catch (error) {
             next(error)
