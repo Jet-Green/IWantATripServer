b0a3a763276f4bb3bf3a13b56454d8cc27f3241f
regtyit <titov54275@gmail.com>
2024-03-26 17:14:19 +0400

    idea

--- controllers/idea-controller.js
+++ controllers/idea-controller.js
@@ -5,7 +5,15 @@ module.exports = {
     async createIdea(req, res, next) {
         try {
             let idea = await ideaModel.create(Object.assign(req.body, { author: req.user._id }))
-            await sendPost(`<b>Новая идея!</b> <b>Тема:</b> ${idea.topic} ${idea.description}`)
+            await sendPost(`
+            <b>Новая идея!</b>
+            <b>Тема:</b> ${idea.topic}
+            <b>Краткое описание:</b> ${idea.offer}
+            <b>Примерная дата:</b> ${idea.date}
+            <b>Маршрут:</b> ${idea.tripRoute}
+            <b>Количество людей:</b> ${idea.maxPeople}
+            <b>Описание:</b> ${idea.description}
+            `)
             res.sendStatus(200)
         } catch (error) {
             next(error)
--- models/idea-model.js
+++ models/idea-model.js
@@ -2,6 +2,10 @@ const { Schema, model } = require('mongoose');
 
 const IdeaSchema = new Schema({
     topic: String,
+    offer: String,
+    date: String,
+    tripRoute: String,
+    maxPeople: String,
     description: String,
     author: { type: Schema.Types.ObjectId, ref: 'User' }
 })
