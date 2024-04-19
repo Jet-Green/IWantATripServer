b5efda8470b7284c122809611853d0019a3d82b4
Yegor Knyazev <egorknyazev10@gmail.com>
2024-04-19 18:01:17 +0400

    done

---
+++ controllers/bus-controller.js
@@ -0,0 +1,21 @@
+const busModel = require("../models/bus-model")
+
+module.exports = {
+    async get(req, res, next) {
+        try {
+            let buses = await busModel.find()
+            console.log('dfsgsgfds')
+            res.status(200).json(buses)
+        } catch (error) {
+            next(error)
+        }
+    },    
+    async create(req, res, next) {
+        try {
+            await busModel.create(req.body)
+            res.sendStatus(200)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
