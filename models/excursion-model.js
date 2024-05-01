101326b7ee30ce1811bc69a284aa55a3d3749508
qbclub <17926057+qbclub@users.noreply.github.com>
2024-05-01 12:14:41 +0400

    add Excurision model

--- models/excursion-model.js
+++ models/excursion-model.js
@@ -2,6 +2,26 @@ const { Schema, model } = require('mongoose')
 
 const ExcursionSchema = new Schema({
     name: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    contacts: { type: Object},
+    time: { type: Schema.Types.ObjectId, ref: 'ExcursionDate'},
+    description: { type: String },
+    location: { type: Schema.Types.ObjectId, ref: 'Location' },
+    duration: { type: String},
+    minPeople:{type: Number},
+    maxPeople: { type: Number},
+    guedes: [{ type: [Schema.Types.ObjectId], ref: 'Guide' }],
+    excursionType: { type: String},
+    startPlace: { type: String },
+    prices: { type: [Object]},
+    images: { type: [String]},
+    minAge: { type: Number},
+    type: { type: Object},
+    deadline: { type: String},
+    requirements: { type: String},
+    availability: { type: String}
+
+
 })
 
 module.exports = model('Excursion', ExcursionSchema);
