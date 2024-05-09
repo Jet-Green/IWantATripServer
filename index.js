693b31e8d3e8851bedb8fd474a3b074283b0e783
JetGreen <grishadzyin@gmail.com>
2024-05-09 14:09:49 +0400

    ch limit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const excursionRouter = require('./routers/excursion-router')
 const { rateLimit } = require('express-rate-limit')
 
 const limiter = rateLimit({
-	windowMs: 5 * 60 * 1000, // 5 minutes
+	windowMs: 15 * 60 * 1000, // 5 minutes
 	limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
