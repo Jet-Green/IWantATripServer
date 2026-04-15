cf92df17468496d5b12b40ab7f7985c6cf6e3d91
Jet-Green <grishadzyin@gmail.com>
2026-04-15 17:49:57 +0500

    add log

--- controllers/yookassa-controller.js
+++ controllers/yookassa-controller.js
@@ -2,6 +2,7 @@ const YooKassaService = require('../service/yookassa-service');
 
 module.exports = {
   async webhook(req, res) {
+    console.log('[yookassa webhook] body:', JSON.stringify(req.body, null, 2));
     const result = YooKassaService.handleNotification(req.body, req.ip);
     return res.sendStatus(result.httpStatus);
   },
